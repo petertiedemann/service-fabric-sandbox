e9d78d268fa1e9d164e914ac9301ffc1e16e4f5f
Peter Tiedemann <pt@configit.com>
2016-12-20 19:26:37 +0100

    First commit

---
+++ StatelessSandbox/GreeterImpl.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+using Grpc.Core;
+
+using Helloworld;
+
+namespace StatelessSandbox {
+  internal class GreeterImpl: Greeter.GreeterBase {
+    // Server side handler of the SayHello RPC
+    public override Task<HelloReply> SayHello( HelloRequest request, ServerCallContext context ) {
+      return Task.FromResult( new HelloReply { Message = "Hello " + request.Name } );
+    }
+  }
+}
---
+++ StatelessSandbox/gRpcListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+
+using Helloworld;
+
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+
+namespace StatelessSandbox {
+  public class GRpcListener : ICommunicationListener
+  {
+    private readonly StatelessServiceContext _ctx;
+
+    private readonly int _port;
+
+    private readonly Server _server;
+
+    private readonly string _ipAddress;
+
+    public GRpcListener( StatelessServiceContext ctx, int port ) {
+      _ctx = ctx;
+      _port = port;
+      _ipAddress = FabricRuntime.GetNodeContext().IPAddressOrFQDN;
+      _server = new Server {
+        Services = { Greeter.BindService( new GreeterImpl() ) },
+        Ports = { new ServerPort( _ipAddress, port, ServerCredentials.Insecure ) }
+      };
+    }
+
+    public Task<string> OpenAsync( CancellationToken cancellationToken ) {
+      return Task.Run( () => {
+        _server.Start();
+        return $"tcp::{_ipAddress}:{_port}";
+      }, cancellationToken );
+    }
+
+    public Task CloseAsync( CancellationToken cancellationToken ) {
+      return _server.ShutdownAsync();
+    }
+
+    public void Abort() {
+      _server.KillAsync().RunSynchronously();
+    }
+  }
+}
